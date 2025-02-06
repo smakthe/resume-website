c776479a653918cb57b543ac402ba8537daa8c1a
smakthe <scmakra99@gmail.com>
2025-02-06 17:29:10 +0530

    This commit is temporary and will be rolled back

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -24,11 +24,12 @@ const Skills = () => (
   <SkillsContainer>
     <Title>Skills</Title>
     <SkillList>
-      <SkillItem><strong>Languages:</strong> Ruby, JavaScript, Python, C, C++, Java, HTML & CSS</SkillItem>
+      <SkillItem><strong>Languages:</strong> Ruby, JavaScript, TypeScript Python, C, C++, Java, HTML & CSS</SkillItem>
+      <SkillItem><strong>Testing Frameworks:</strong> Rspec, E2E</SkillItem>
       <SkillItem><strong>Relational Databases:</strong> MySQL, PostgreSQL</SkillItem>
       <SkillItem><strong>Non-relational Databases:</strong> MongoDB, Redis</SkillItem>
       <SkillItem><strong>MVC Framework:</strong> Ruby on Rails</SkillItem>
-      <SkillItem><strong>Meta-Frameworks and Libraries:</strong> React.js, Node.js, Express.js, Next.js, Sveltte</SkillItem>
+      <SkillItem><strong>Meta-Frameworks and Libraries:</strong> React.js, Node.js, Express.js, Nest.js, Sveltte</SkillItem>
       <SkillItem><strong>AWS Technologies:</strong> DynamoDB, EC2, IoT, Lambda, S3, SNS, SQS, Cognito</SkillItem>
       <SkillItem><strong>CI/CD and Devops:</strong> Git, GitHub, GitHub Action, Docker, Kubernetes</SkillItem>
     </SkillList>
